c86cf1655792ab8d499c78e3ce1a5a092952cd6f
Misplayz <98254147+Misplayz@users.noreply.github.com>
2024-03-11 23:53:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
-const keep_alive = require('./keep_alive.js')
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
