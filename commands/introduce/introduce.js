50dcfed861fabcb72c3d79d6c4eb3c3db2886fba
Misplayz <98254147+Misplayz@users.noreply.github.com>
2024-05-03 11:01:52 +0700

    Update introduce.js

--- commands/introduce/introduce.js
+++ commands/introduce/introduce.js
@@ -1,107 +1,97 @@
-const { Client, EmbedBuilder, SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { Client, MessageEmbed, SlashCommandBuilder, MessageActionRow, MessageButton, ButtonInteraction, ButtonInteractionType } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('introduce')
         .setDescription('Allows a user to introduce themselves.')
-        .addStringOption(option => 
+        .addStringOption(option =>
             option.setName('nickname')
-            .setDescription('Enter your nickname')
-            .setRequired(true))
-        .addStringOption(option => 
+                .setDescription('Enter your nickname')
+                .setRequired(true))
+        .addStringOption(option =>
             option.setName('age')
-            .setDescription('Enter your age')
-            .setRequired(true))
-        .addStringOption(option => 
+                .setDescription('Enter your age')
+                .setRequired(true))
+        .addStringOption(option =>
             option.setName('gender')
-            .setDescription('Enter your gender')
-            .setRequired(true)),
+                .setDescription('Enter your gender')
+                .setRequired(true)),
 
     async execute(interaction) {
-        // ตรวจสอบว่าบอทอยู่ในห้องที่ต้องการหรือไม่
-            if (interaction.channel.name !== '👤-𝕚𝕟𝕥𝕣𝕠𝕕𝕦𝕔𝕖') {
-                await interaction.reply({ content: 'You cannot use this command in this channel.', ephemeral: true });
-                    return;
-                 }
-            
-            const userNickname = interaction.options.getString('nickname');
-            const userAge = interaction.options.getString('age');
-            const userGender = interaction.options.getString('gender');
-            
-            // แท็กผู้ใช้ในรูปแบบข้อความ
-            const taggedUser = interaction.user.toString();
-            
-            const messageContent = `Please confirm your introduction by clicking one of the buttons below.`;
-            
-            const confirm = new ButtonBuilder()
-                .setCustomId('confirm')
-                .setLabel('Confirm')
-                .setStyle(ButtonStyle.Success);
-                
-            const cancel = new ButtonBuilder()
-                .setCustomId('cancel')
-                .setLabel('Cancel')
-                .setStyle(ButtonStyle.Secondary);
-                
-            const row = new ActionRowBuilder()
-                .addComponents(cancel, confirm);
-                
-            // สร้าง Embed
-            const embed = new EmbedBuilder()
-                .setTitle(`Please confirm your introduction`)
-                .setColor('#00FFFF')
-                .addFields(
-                    { name: 'Nickname', value: userNickname},
-                    { name: 'Age', value: userAge},
-                    { name: 'Gender', value: userGender}
-                )
-                .setThumbnail(interaction.user.avatarURL({ dynamic: true })) // แก้ไขที่นี่
-                .setTimestamp();
-                
-            // ส่งข้อความและ Embed พร้อมกัน
+        // Check if the interaction is in the correct channel
+        if (interaction.channel.name !== '👤-𝕚𝕟𝕥𝕣𝕠𝕕𝕦𝕔𝕖') {
+            await interaction.reply({ content: 'You cannot use this command in this channel.', ephemeral: true });
+            return;
+        }
+
+        const userNickname = interaction.options.getString('nickname');
+        const userAge = interaction.options.getString('age');
+        const userGender = interaction.options.getString('gender');
+        const taggedUser = interaction.user.toString();
+
+        const messageContent = `Please confirm your introduction by clicking one of the buttons below.`;
+
+        const confirmButton = new MessageButton()
+            .setCustomId('confirm')
+            .setLabel('Confirm')
+            .setStyle('SUCCESS');
+
+        const cancelButton = new MessageButton()
+            .setCustomId('cancel')
+            .setLabel('Cancel')
+            .setStyle('SECONDARY');
+
+        const row = new MessageActionRow()
+            .addComponents(cancelButton, confirmButton);
+
+        const embed = new MessageEmbed()
+            .setTitle(`Please confirm your introduction`)
+            .setColor('#00FFFF')
+            .addFields(
+                { name: 'Nickname', value: userNickname },
+                { name: 'Age', value: userAge },
+                { name: 'Gender', value: userGender }
+            )
+            .setThumbnail(interaction.user.avatarURL({ dynamic: true }))
+            .setTimestamp();
+
         const initialMessage = await interaction.reply({ content: messageContent, embeds: [embed], components: [row], ephemeral: true });
 
-        // กำหนดเวลาในการรอการยืนยัน
-        const filter = i => i.customId === 'confirm' || i.customId === 'cancel';
-        const collector = interaction.channel.createMessageComponentCollector({ filter, time: 60_000 });
+        const filter = (i) => i.isButton() && (i.customId === 'confirm' || i.customId === 'cancel');
+        const collector = interaction.channel.createMessageComponentCollector({ filter, time: 60000 });
 
-        collector.on('collect', async i => {
+        collector.on('collect', async (i) => {
             if (i.customId === 'confirm') {
                 const messageContent = `${taggedUser}`;
-                const indEmbed = new EmbedBuilder()
+                const indEmbed = new MessageEmbed()
                     .setTitle(`Introduce by ${interaction.user.tag} has been confirmed.`)
-                    .setThumbnail(interaction.user.avatarURL({ dynamic: true })) // แก้ไขที่นี่
+                    .setThumbnail(interaction.user.avatarURL({ dynamic: true }))
                     .setColor('#00FFFF')
                     .addFields(
-                        { name: 'Nickname', value: userNickname},
-                        { name: 'Age', value: userAge},
-                        { name: 'Gender', value: userGender}
+                        { name: 'Nickname', value: userNickname },
+                        { name: 'Age', value: userAge },
+                        { name: 'Gender', value: userGender }
                     )
                     .setTimestamp();
-            
-                // ส่ง Embed ที่ถูกอัพเดตแล้ว และลบปุ่ม
+
                 await interaction.deleteReply();
-                const targetChannel = interaction.guild.channels.cache.get('1152979349461553182');
+                const targetChannel = interaction.guild.channels.cache.get('1152979594337599579');
                 if (targetChannel) {
                     await targetChannel.send({ content: messageContent, embeds: [indEmbed] });
 
-                    // เพิ่มบทบาทใหม่และลบบทบาทเก่า
                     const member = await interaction.guild.members.fetch(interaction.user);
                     const newRole = interaction.guild.roles.cache.find(role => role.name === 'Family-Member');
-                    //const oldRole = interaction.guild.roles.cache.find(role => role.name === 'OLD_ROLE_NAME');
                     if (newRole) await member.roles.add(newRole);
-                    //if (oldRole) await member.roles.remove(oldRole);
                 } else {
                     console.log('Target channel not found.');
                 }
             } else if (i.customId === 'cancel') {
                 await i.update({ content: 'Introduction cancelled.', embeds: [], components: [] });
             }
-            
         });
 
-        collector.on('end', collected => {
+        collector.on('end', (collected) => {
             if (collected.size === 0) interaction.editReply({ content: 'No confirmation received within the time limit, cancelling introduction.', components: [] });
         });
     },
-}
+};
