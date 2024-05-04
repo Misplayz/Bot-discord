8742f905c23e512d9ee583d161824fb3fa720169
Misplayz <98254147+Misplayz@users.noreply.github.com>
2024-05-04 21:00:33 +0700

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,35 +1,6 @@
-const http = require('http');
-const axios = require('axios');
+var http = require('http');
 
-const PORT = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write("Now I'm alive");
+http.createServer(function (req, res) {
+  res.write("I'm alive");
   res.end();
-});
-
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-setInterval(() => {
-  console.log("Server is still running...");
-}, 60 * 1000);
-
-// เมื่อมีคำขอ GET มาที่ /sendRequest
-server.on('request', async (req, res) => {
-  if (req.method === 'GET' && req.url === '/sendRequest') {
-    try {
-      // ส่งคำขอ GET ไปยังเซิร์ฟเวอร์อื่น
-      const response = await axios.get('http://example.com/api/data');
-      // ส่งข้อมูลกลับไปยังผู้ใช้
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(response.data));
-    } catch (error) {
-      console.error('Error:', error);
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('Internal Server Error');
-    }
-  }
-});
+}).listen(8080);
